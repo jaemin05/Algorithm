c9eeef7c043617b646d52dcc6dd681aa33bbe760
jaemin <sonjm3518@dsm.hs.kr>
2022-07-07 22:53:11 +0900

    📑 :: GetReportResults

---
+++ programmers/level1/GetReportResults .java
@@ -0,0 +1,11 @@
+﻿class GetReportResults {
+    public int solution(String s) {
+        String[] answer = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < answer.length; i++) {
+            s = s.replaceAll(answer[i], String.valueOf(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+}
