8f0bc42a892d4692646886d5e7dcd6b467e097c6
jaemin <sonjm3518@dsm.hs.kr>
2022-07-19 09:16:13 +0900

    📑 :: Correct Parenthesis

---
+++ programmers/level2/CorrectParenthesis.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        answer = (stack.isEmpty()) ? true : false;
+        return answer;
+    }
+}
