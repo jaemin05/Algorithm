1f8349dd7397219f7288df9e59be82b044920ca4
jaemin05 <89118999+jaemin05@users.noreply.github.com>
2022-07-22 12:45:26 +0900

    [level 2] Title: 타겟 넘버, Time: 0.34 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,20 @@
+class Solution {
+    
+    int result = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(0, numbers, 0, target);
+        return result;
+    }
+    
+    public void dfs(int i, int[] numbers, int sum, int target) {
+        
+        if(i == numbers.length) {
+            result += (sum == target ? 1:0);
+            return;
+        }
+        
+        dfs(i+1, numbers, sum + numbers[i], target);
+        dfs(i+1, numbers, sum - numbers[i], target);
+    }
+}
