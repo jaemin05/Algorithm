927a9d7470a1488daa9fa9cf8772a752b0141e4e
jaemin05 <89118999+jaemin05@users.noreply.github.com>
2022-07-29 16:36:00 +0900

    [Gold V] Title: 토마토, Time: 728 ms, Memory: 125560 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,77 +1,81 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
-      BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st = new StringTokenizer(bf.readLine());
-      
-      int result = 0;     
-      int notYet = 0;      
-        
-      int M = Integer.parseInt(st.nextToken());
-      int N = Integer.parseInt(st.nextToken());
-        
-      int[][] days = new int[N][M];     
-      int[][] tomatos = new int[N][M];  
-        
-      Queue<Pair> qu = new LinkedList<>();
-        
-      int[] dx = {1, 0 , -1, 0};   
-      int[] dy = {0, 1, 0, -1};
-      
-      for(int i=0; i<N; i++){
-          st = new StringTokenizer(bf.readLine());
-          for(int j=0; j<M; j++){
-              days[i][j] = -1;  
-              int num = Integer.parseInt(st.nextToken());
-              tomatos[i][j] = num;
-              if(num == 1){
-                  qu.offer(new Pair(i, j)); 
-                  days[i][j] = 0;           
-              }else if(num == 0){
-                  notYet++;  
-              }
-          }
-      }
-      
-      if(notYet == 0){
-          System.out.print(0);
-          System.exit(0);
-      }
-      
-      while(!qu.isEmpty()){
-          Pair p = qu.poll();
-          
-          for(int i=0; i<4; i++){
-              int nX = p.x + dx[i];
-              int nY = p.y + dy[i];
-              
-              if(nX <0 || nX >= N || nY < 0 || nY >= M){
-                  continue;
-              }
-              if(tomatos[nX][nY] == -1 || days[nX][nY] != -1){
-                  continue;
-              }
-              qu.offer(new Pair(nX, nY)); notYet--;
-              days[nX][nY] = days[p.x][p.y] + 1;
-              if(result < days[nX][nY]){
-                  result = days[nX][nY];
-              }
-          }
-      }
-      
-      if(notYet > 0){
-          System.out.print(-1);
-      }else{
-          System.out.print(result);
-      }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int day = 0;
+        int notTomato = 0;
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] days = new int[n][m];
+        int[][] tomato = new int[n][m];
+
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        Queue<Pair> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                days[i][j] = -1;
+                int num = Integer.parseInt(st.nextToken());
+                tomato[i][j] = num;
+
+                if (num == 1) {
+                    queue.offer(new Pair(i, j));
+                    days[i][j] = 0;
+                } else if (num == 0) {
+                    notTomato++;
+                }
+            }
+        }
+
+        if (notTomato == 0) {
+            System.out.println(0);
+            System.exit(0);
+        }
+
+        while (!queue.isEmpty()) {
+            Pair pair = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = pair.x + dx[i];
+                int ny = pair.y + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || tomato[nx][ny] == -1 || days[nx][ny] != -1) {
+                    continue;
+                }
+
+                queue.offer(new Pair(nx, ny));
+                notTomato--;
+
+                days[nx][ny] = days[pair.x][pair.y] + 1;
+
+                if (day < days[nx][ny]) {
+                    day = days[nx][ny];
+                }
+            }
+        }
+
+        if (notTomato > 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(day);
+        }
     }
-    
-    public static class Pair{
-        int x, y;
-        
-        public Pair(int x, int y){
+
+    public static class Pair {
+        int x;
+        int y;
+
+        public Pair(int x, int y) {
             this.x = x;
             this.y = y;
         }
